ccf8df2f7d75376cea1141ec54cbe5d8980a09ca
droid <droid@debian-BULLSEYE-live-builder-AMD64>
2024-01-09 12:25:14 +0000

    update to title

--- item/models.py
+++ item/models.py
@@ -3,6 +3,8 @@ from container.models import Container
 class Item(models.Model):
     title = models.CharField(max_length=100, blank=True, default='')
     container = models.ForeignKey(Container, on_delete=models.CASCADE, blank=True, null=True)
+    kijiji_listing = models.CharField(max_length=1000, blank=True, default='')
+    facebook_listing = models.CharField(max_length=1000, blank=True, default='')
 
     def __str__(self):
         return self.title
